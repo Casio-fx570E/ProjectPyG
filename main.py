575de008dcbfd3ffdb39d5e0496ef2abd5713b98
CompVlad <tvr2007@mail.ru>
2022-12-30 16:18:21 +0300

    Background

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ screen_width = 1200
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+background_image = pygame.image.load('data/Background/BG3.png')
+background_image_2 = pygame.image.load('data/Background/BG1.png')
+background_image_3 = pygame.image.load('data/Background/BG2.png')
+image1 = pygame.transform.scale(background_image, (1200, 700))
+image2 = pygame.transform.scale(background_image_2, (1200, 700))
+image3 = pygame.transform.scale(background_image_3, (1200, 700))
 
 while True:
     for event in pygame.event.get():
@@ -12,7 +18,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill('cyan')
-
+    screen.blit(image2, (0, 0))
+    screen.blit(image3, (0, 0))
+    screen.blit(image1, (0, 0))
     pygame.display.update()
     clock.tick(60)
