3c0ec2de295a2107d0b108a3ee395d1d22c2c281
mems <mr.vlastecs@gmail.com>
2023-01-09 01:22:36 +0500

    Начальный экран

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pygame, sys
+import pygame, sys, os
 from map import *
 from level import Level
+
 
 pygame.init()
 screen_width = 1200
@@ -18,15 +19,70 @@ image1 = pygame.transform.scale(background_image, (1200, 700))
 image2 = pygame.transform.scale(background_image_2, (1200, 700))
 image3 = pygame.transform.scale(background_image_3, (1200, 700))
 
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не удаётся загрузить:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if color_key is not None:
+        color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ['Играть',
+                  'Правила - их нет']
+
+    fon = pygame.transform.scale(load_image('fon/fon.jpg'), (1200, 700))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('#8ffe09'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+                terminate()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+start_screen()
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+
     screen.blit(image2, (0, 0))
     screen.blit(image3, (0, 0))
     screen.blit(image1, (0, 0))
+
     level.run()
     pygame.display.update()
     clock.tick(FPS)
---
+++ screen.py
@@ -0,0 +1,54 @@
+import pygame
+import sys
+import os
+from main import *
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не удаётся загрузить:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if color_key is not None:
+        if color_key is -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Перемещение героя", "",
+                  "",
+                  "На торе"]
+
+    fon = pygame.transform.scale(load_image('fon/fon.jpg'), (1200, 700))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+
+        pygame.display.flip()
+        clock.tick(FPS)
