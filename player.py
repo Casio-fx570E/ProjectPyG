13321d22a6b43bf9ac3ac7f19ed97414e722e090
CompVlad <tvr2007@mail.ru>
2023-01-10 16:16:24 +0300

    Random mob movement

--- player.py
+++ player.py
@@ -69,9 +69,10 @@ class Player(pygame.sprite.Sprite):
                 self.frames_idle_left_count, self.frames_idle_left = self.animated_move(self.frames_idle_left_count,
                                                                                         self.frames_idle_left)
         if keys[pygame.K_SPACE] and self.jumped == 0:
-            self.jumped = 1
-            self.image = load_image('Character/Jump/Jump.png')
-            self.jump_yes()
+            if self.direction.y == 0:
+                self.jumped = 1
+                self.image = load_image('Character/Jump/Jump.png')
+                self.jump_yes()
 
         if keys[pygame.K_SPACE] == False:
             self.jumped = 0
