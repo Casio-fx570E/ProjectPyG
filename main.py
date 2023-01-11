eed465980c56b8a399a3d6d98072487fd3790be8
mems <mr.vlastecs@gmail.com>
2023-01-11 18:52:53 +0500

    фоновая музыка

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, sys
 from map import *
 from level import Level
 from load_pic import load_image
+from musica import fmusic
 
 pygame.init()
 screen_width = 1200
@@ -16,6 +17,7 @@ background_image = pygame.image.load('data/Background2.0/BG1.png')
 background_image_2 = pygame.image.load('data/Background2.0/BG2.png')
 image1 = pygame.transform.scale(background_image, (1200, 700))
 image2 = pygame.transform.scale(background_image_2, (1200, 700))
+fmusic()
 
 
 def play():
